50e143c9755a487f3983238f834146d4dffe9d86
Charles Arnold <charles.arnold@gisinc.com>
2017-11-14 18:46:02 -0800

    updates for 2nd time around

--- Up2Interpretation_ctypes/bitmap_greyscale.c
+++ Up2Interpretation_ctypes/bitmap_greyscale.c
@@ -3,86 +3,85 @@
 #include <stdio.h>
 #include <string.h>
 
-
 /**
  * Plucked From http://stackoverflow.com/questions/2654480/writing-bmp-image-in-pure-c-c-without-other-libraries
  */
-void read_grayscale_bmp(char *filename, int w, int h, unsigned char **img_out) {
-
+void read_grayscale_bmp(char *filename, int w, int h, unsigned char **img_out)
+{
     unsigned char *img = NULL;
 
     FILE *f;
     unsigned char ignored[40];
     const size_t bmppad_sz = (4 - (w * 3) % 4) % 4;
 
-    if(img)
+    if (img)
         free(img);
-    img = (unsigned char *)malloc(3*w*h);
-    memset(img, 0, 3*w*h);
+    img = (unsigned char *)malloc(3 * w * h);
+    memset(img, 0, 3 * w * h);
 
     f = fopen(filename, "rb");
     fread(ignored, 1, 14, f);
     fread(ignored, 1, 40, f);
-    for(int i=0; i<h; i++)
+    for (int i = 0; i < h; i++)
     {
-        fread(img+(w*(h-i-1)*3), 3, w, f);
+        fread(img + (w * 3 * (h - i - 1)), 3, w, f);
         fread(ignored, 1, bmppad_sz, f);
     }
     fclose(f);
 
-    for(int i=0; i < w; i++)
+    for (int i = 0; i < w; i++)
     {
-        for(int j=0; j < h; j++)
+        for (int j = 0; j < h; j++)
         {
-            int y=(h-1)-j;
+            int y = h - j - 1;
 
             // Calculate "luma" from RGB
             // http://homepages.inf.ed.ac.uk/rbf/CVonline/LOCAL_COPIES/POYNTON1/ColorFAQ.html#RTFToC18
-            float r = img[(i+y*w)*3+2];
-            float g = img[(i+y*w)*3+1];
-            float b = img[(i+y*w)*3+0];
+            float r = img[(i + y * w) * 3 + 2];
+            float g = img[(i + y * w) * 3 + 1];
+            float b = img[(i + y * w) * 3 + 0];
 
-            float ciey = .212671*r + .715160*g + .072169*b;
-            img[(i+y*w)*3+2] = img[(i+y*w)*3+1] = img[(i+y*w)*3+0] = ciey;
+            float ciey = .212671 * r + .715160 * g + .072169 * b;
+            img[(i + y * w) * 3 + 2] = img[(i + y * w) * 3 + 1] = img[(i + y * w) * 3 + 0] = ciey;
         }
     }
 
     *img_out = img;
 }
 
-void write_bmp(char *filename, unsigned char *img, int w, int h) {
+void write_bmp(char *filename, unsigned char *img, int w, int h)
+{
 
     FILE *f;
 
-    unsigned char bmpfileheader[14] = {'B','M', 0,0,0,0, 0,0, 0,0, 54,0,0,0};
-    unsigned char bmpinfoheader[40] = {40,0,0,0, 0,0,0,0, 0,0,0,0, 1,0, 24,0};
-    unsigned char bmppad[3] = {0,0,0};
+    unsigned char bmpfileheader[14] = {'B', 'M', 0, 0, 0, 0, 0, 0, 0, 0, 54, 0, 0, 0};
+    unsigned char bmpinfoheader[40] = {40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 24, 0};
+    unsigned char bmppad[3] = {0, 0, 0};
     const size_t bmppad_sz = (4 - (w * 3) % 4) % 4;
-    
-    int filesize = 54 + 3*w*h;  //w is your image width, h is image height, both int
 
-    bmpfileheader[ 2] = (unsigned char)(filesize    );
-    bmpfileheader[ 3] = (unsigned char)(filesize>> 8);
-    bmpfileheader[ 4] = (unsigned char)(filesize>>16);
-    bmpfileheader[ 5] = (unsigned char)(filesize>>24);
+    int filesize = 54 + 3 * w * h; //w is your image width, h is image height, both int
 
-    bmpinfoheader[ 4] = (unsigned char)(       w    );
-    bmpinfoheader[ 5] = (unsigned char)(       w>> 8);
-    bmpinfoheader[ 6] = (unsigned char)(       w>>16);
-    bmpinfoheader[ 7] = (unsigned char)(       w>>24);
-    bmpinfoheader[ 8] = (unsigned char)(       h    );
-    bmpinfoheader[ 9] = (unsigned char)(       h>> 8);
-    bmpinfoheader[10] = (unsigned char)(       h>>16);
-    bmpinfoheader[11] = (unsigned char)(       h>>24);
+    bmpfileheader[2] = (unsigned char)(filesize);
+    bmpfileheader[3] = (unsigned char)(filesize >> 8);
+    bmpfileheader[4] = (unsigned char)(filesize >> 16);
+    bmpfileheader[5] = (unsigned char)(filesize >> 24);
+
+    bmpinfoheader[4] = (unsigned char)(w);
+    bmpinfoheader[5] = (unsigned char)(w >> 8);
+    bmpinfoheader[6] = (unsigned char)(w >> 16);
+    bmpinfoheader[7] = (unsigned char)(w >> 24);
+    bmpinfoheader[8] = (unsigned char)(h);
+    bmpinfoheader[9] = (unsigned char)(h >> 8);
+    bmpinfoheader[10] = (unsigned char)(h >> 16);
+    bmpinfoheader[11] = (unsigned char)(h >> 24);
 
     f = fopen(filename, "wb");
-    fwrite(bmpfileheader,1,14,f);
-    fwrite(bmpinfoheader,1,40,f);
-    for(int i=0; i<h; i++)
+    fwrite(bmpfileheader, 1, 14, f);
+    fwrite(bmpinfoheader, 1, 40, f);
+    for (int i = 0; i < h; i++)
     {
-        fwrite(img+(w*(h-i-1)*3), 3, w, f);
+        fwrite(img + (w * (h - i - 1) * 3), 3, w, f);
         fwrite(bmppad, 1, bmppad_sz, f);
     }
     fclose(f);
 }
-
--- Up2Interpretation_ctypes/bitmap_greyscale.py
+++ Up2Interpretation_ctypes/bitmap_greyscale.py
@@ -1,12 +1,68 @@
 #!/usr/bin/env python
 
+import time
 import ctypes
+import logging
 
+# This could be read from the header, but I just hard coded it here for easy comparison
 w, h = 2209, 1657
+fn = './mountain-spring.bmp'
+outfn = './test.bmp'
 
-lh=ctypes.CDLL('./bitmap_greyscale.so')
+def ctypes_grayscale(fn, w, h, outfn):
+    lh = ctypes.CDLL('./bitmap_greyscale.so')
+    imgbuf = ctypes.POINTER(ctypes.c_ubyte)()
+    lh.read_grayscale_bmp(fn, w, h, ctypes.byref(imgbuf))
+    lh.write_bmp(outfn, imgbuf, w, h)
 
-imgbuf = ctypes.POINTER(ctypes.c_ubyte)()
-lh.read_grayscale_bmp('./mountain-spring.bmp', w, h, ctypes.byref(imgbuf))
-lh.write_bmp('./test.bmp', imgbuf, w, h)
+def python_grayscale(fn, w, h, outfn):
+    header_sz = 54
+    bmppad_sz = (4 - (w * 3) % 4) % 4
+    row_sz = w * 3
 
+    with open(fn, 'rb') as fp:
+        imgbytes = fp.read()
+    
+    img_rows = []
+    for row_idx in range(h):
+        row_start_idx = header_sz + (row_sz + bmppad_sz) * row_idx
+        img_rows.append(imgbytes[row_start_idx:row_start_idx + row_sz])
+    # assert len(img_rows) == h
+    # assert all(len(row) == w * 3 for row in img_rows)
+
+    grayscale_rows = []
+    for row in img_rows:
+        grayscale_row = []
+        for pix_idx in range(0, w * 3, 3):
+            pix_bytes = row[pix_idx:pix_idx + 3]
+            b, g, r = map(ord, pix_bytes)
+            ciey = .212671 * r + .715160 * g + .072169 * b
+            grayscale_pix_bytes = chr(int(ciey)) * 3
+            grayscale_row.append(grayscale_pix_bytes)
+        grayscale_rows.append(''.join(grayscale_row))
+    # assert len(grayscale_rows) == h
+    # assert all(len(row) == w * 3 for row in grayscale_rows)
+
+    with open(outfn, 'wb') as outfp:
+        outfp.write(imgbytes[:header_sz])
+        for row in grayscale_rows:
+            outfp.write(row)
+            outfp.write('\0' * bmppad_sz)
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    
+    ctypes_grayscale(fn, w, h, './ctypes_test.bmp')
+    ctypes_time = time.time() - start_time
+
+    python_grayscale(fn, w, h, './py_test.bmp')
+    py_time = time.time() - start_time
+
+    logging.basicConfig(level=logging.INFO)
+    logging.info("""
+    ctypes time: {}
+    python time: {}
+    diff: {}
+    % diff: {}
+    """.format(ctypes_time, py_time, py_time - ctypes_time, (py_time - ctypes_time) / py_time))
